1af53c591bd73ab5031f0bca6d572319bf34f784
caoqiqi <caoqi95@gmail.com>
2018-10-01 16:26:17 +0900

    Update PhysicalPixel

---
+++ code/PhysicalPixel/physicalPixel.ino
@@ -0,0 +1,22 @@
+const int ledPin = 13;
+int incomingByte;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  if(Serial.available()>0){
+    incomingByte = Serial.read();
+    if(incomingByte == 'l'){
+      digitalWrite(ledPin, LOW);
+      }
+
+    if(incomingByte == 'h'){
+      digitalWrite(ledPin, HIGH);
+      }
+    }
+
+}
